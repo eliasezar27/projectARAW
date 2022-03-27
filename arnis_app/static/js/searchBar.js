002886b533d4a8c0d12bf8311d62ecbc5a8582f3
Elias <eliasezar27@gmail.com>
2022-03-27 16:12:43 +0800

    ver 0.9.14
    ADDED:
    Search bar functionality to search either within teacher's list or section's list.
    
    FIXED:
    Search bar input group.
    
    TODO:
    Admin dashboard visualization.
    
    Track parent on strand table.
    
    Admin profile settings (upload profile pic and change name options)
    
    Prioritize admin module views design and functionalities.
    Add and design teacher module:
    access teacher  and student database for section cards UI.

---
+++ arnis_app/static/js/searchBar.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+     $('#searchBar1, #searchBar2').on('keyup focus', function() {
+          var input = $(this).val();
+          var filter = input.toLowerCase();
+          var selectSearch = $("#selectSearch").val();
+          var selectSearch2 = $("#selectSearch2").val();
+
+          console.log(selectSearch);
+
+          var card1 = $('#teacherList .col-md-6');
+          var card2 = $('#sectionList .col-md-6');
+
+           let card;
+           let otherCard;
+          switch(selectSearch) {
+            case '0':
+                card1.css('display', 'block');
+                card2.css('display', 'block');
+                break;
+              case '1':
+                card = card1;
+                otherCard = card2;
+                otherCard.css('display', 'block');
+                break;
+              case '2':
+                card = card2;
+                otherCard = card1;
+                otherCard.css('display', 'block');
+                break;
+            }
+
+            switch(selectSearch2) {
+            case '0':
+                card1.css('display', 'block');
+                card2.css('display', 'block');
+                break;
+              case '1':
+                card = card1;
+                otherCard = card2;
+                otherCard.css('display', 'block');
+                break;
+              case '2':
+                card = card2;
+                otherCard = card1;
+                otherCard.css('display', 'block');
+                break;
+            }
+
+//          For teacher's list
+            if(selectSearch == '1' || selectSearch == '2' || selectSearch2 == '1' || selectSearch2 == '2'){
+              for (i = 0; i < card.length; i++) {
+                if (card[i].innerText.toLowerCase().includes(filter)) {
+                  card[i].style.display = "block";
+                } else {
+                  card[i].style.display = "none";
+                  otherCard.css('display', 'none');
+                }
+              }
+            }
+    });
+
+});
