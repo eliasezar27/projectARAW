bde5ce9c9325581fe01ccfc2233492f1a705ec05
Elias <eliasezar27@gmail.com>
2022-04-06 13:26:05 +0800

    ver 0.9.31
    ADDED:
    Admin module completed!
    
    FIXED:
    
    TODO:
    Teacher module
    
    Complete student module.
    
    Design suggestions by sir rex.
    
    Assign other teacher as admin.

--- arnis_app/static/js/getSectionListPerTeacher.js
+++ arnis_app/static/js/getSectionListPerTeacher.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
                     if (data1.result == 'success'){
                         for (let i = 0; i < data1.sectionList.length; i++){
                             data1.sectionList[i]['actions'] = "<a class='viewSectionStudentList px-2 py-2' section_id=" + data1.sectionList[i]['section_id'] + " style='cursor: pointer; text-decoration: none;' data-bs-toggle='modal' data-bs-target='#sectionStudentList'>"+
-                                                                    "<i class='fas fa-solid fa-user-group' data-bs-toggle='tooltip' data-bs-placement='top' title='More Info..' style='color: #c46d38;'></i>" +
+                                                                    "<i class='fas fa-solid fa-user-group' data-bs-toggle='tooltip' data-bs-placement='top' title='List of students' style='color: #c46d38;'></i>" +
                                                                 "</a>"
                         }
 //                        console.log(data1.sectionList);
